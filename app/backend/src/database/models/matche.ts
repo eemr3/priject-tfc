0ff62b71f51ddbf626502c7357cdf67a7f6836f7
eemr3 <eemr3@yahoo.com.br>
2022-05-30 22:30:03 -0300

    feat: cria model matche.ts
    model para a tabela de matches

---
+++ app/backend/src/database/models/matche.ts
@@ -0,0 +1,63 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Matche extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: boolean;
+}
+
+Matche.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    // ... Outras configs
+    underscored: true,
+    sequelize: db,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+
+/**
+ * `Workaround` para aplicar as associations em TS:
+ * Associations 1:N devem ficar em uma das instâncias de modelo
+ * */
+
+// Team.belongsTo(Matche, { foreignKey: 'homeTeam', as: 'homeTeams' });
+// Team.belongsTo(Matche, { foreignKey: 'awayTeam', as: 'awayTeams' });
+
+// Matche.hasMany(Team, { foreignKey: 'id', as: 'homeTeams' });
+// Matche.hasMany(Team, { foreignKey: 'id', as: 'awayTeams' });
+
+export default Matche;
