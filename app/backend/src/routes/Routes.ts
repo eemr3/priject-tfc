fd57b44bebfbe2c51e5b6d8821555399a8d3df3e
eemr3 <eemr3@yahoo.com.br>
2022-05-31 22:54:40 -0300

    feat: criar class de rotas Routes.ts

---
+++ app/backend/src/routes/Routes.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import ValidateJoi from '../middleware/validateJoiSchemas';
+import JoiSchemas from '../schemas/joiSchemas';
+import LoginController from '../controllers/Login.controller';
+
+class Routes {
+  private _controller = new LoginController();
+  private _joiSchemas = new JoiSchemas();
+  private _validate = new ValidateJoi();
+
+  login(app: express.Application) {
+    app.post(
+      '/login',
+      (req, res, next) => this._validate.validate(req, res, next, this._joiSchemas.user),
+      (req, res) => this._controller.loginApp(req, res),
+    );
+  }
+}
+
+export default Routes;
