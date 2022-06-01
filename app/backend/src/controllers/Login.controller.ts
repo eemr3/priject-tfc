72176af866a18324e073123144d653ded5c9da7c
Emerson <eemr3@yahoo.com.br>
2022-06-01 18:24:22 -0300

    refactor: altera o controller de login passando a criação do token para a service

--- app/backend/src/controllers/Login.controller.ts
+++ app/backend/src/controllers/Login.controller.ts
@@ -1,21 +1,18 @@
 import { Request, Response } from 'express';
 import CustonError from '../utils/errroF';
-import Token from '../auth/token';
 import Login from '../services/Login.service';
 
 class LoginController {
   private _service = new Login();
-  private _token = new Token();
 
   async loginApp(req: Request, res: Response) {
     const { email, password } = req.body;
     try {
       const user = await this._service.getLogin(email, password);
-      const token = this._token.createToken(user);
 
-      return res.status(200).json({ user, token });
+      return res.status(200).json(user);
     } catch (error) {
-      res.status((error as CustonError).status).json({ message: error });
+      res.status((error as CustonError).status).json({ message: (error as Error).message });
     }
   }
 }
