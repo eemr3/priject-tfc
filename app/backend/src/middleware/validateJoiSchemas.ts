c2a7d8b658bb5371b673305e957363c817405d4d
Emerson <eemr3@yahoo.com.br>
2022-06-01 18:23:33 -0300

    feat: cria middleware de validação para a rota de login

--- app/backend/src/middleware/validateJoiSchemas.ts
+++ app/backend/src/middleware/validateJoiSchemas.ts
@@ -4,8 +4,13 @@ import { Schema } from 'joi';
 class ValidateJoi {
   validate = (req: Request, res: Response, next: NextFunction, schemas: Schema) => {
     const { error } = schemas.validate(req.body);
+
+    if (error?.details[0].type === 'string.email') {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
+
     if (error) {
-      return res.status(400).json(error.details[0].message);
+      return res.status(400).json({ message: error.details[0].message });
     }
 
     next();
