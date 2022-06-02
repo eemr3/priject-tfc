2ea1806d550fc15b131d15cc383f9dc090815158
Emerson <eemr3@yahoo.com.br>
2022-06-01 22:04:18 -0300

    feat: adiciona rota de login/validate

--- app/backend/src/routes/Routes.ts
+++ app/backend/src/routes/Routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import AuthMiddleware from '../middleware/authMiddleware';
 import ValidateJoi from '../middleware/validateJoiSchemas';
 import JoiSchemas from '../schemas/joiSchemas';
 import LoginController from '../controllers/Login.controller';
@@ -7,12 +8,18 @@ class Routes {
   private _controller = new LoginController();
   private _joiSchemas = new JoiSchemas();
   private _validate = new ValidateJoi();
+  private _auth = new AuthMiddleware();
 
   login(app: express.Application) {
     app.post(
       '/login',
       (req, res, next) => this._validate.validate(req, res, next, this._joiSchemas.login),
       (req, res) => this._controller.loginApp(req, res),
+    );
+    app.get(
+      '/login/validate',
+      (req, res, next) => this._auth.validate(req, res, next),
+      (req, res) => this._controller.validateLogin(req, res),
     );
   }
 }
