d8580f2c9a8ada205a63b50d4a1c68058e228a5f
Emerson <eemr3@yahoo.com.br>
2022-06-01 22:04:03 -0300

    feat: adiciona controler para rota de login/validate

--- app/backend/src/controllers/Login.controller.ts
+++ app/backend/src/controllers/Login.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-import CustonError from '../utils/errroF';
+import CustonError from '../utils/errorBase';
 import Login from '../services/Login.service';
 
 class LoginController {
   private _service = new Login();
+  private _role: object;
 
   async loginApp(req: Request, res: Response) {
     const { email, password } = req.body;
@@ -13,6 +14,15 @@ class LoginController {
       return res.status(200).json(user);
     } catch (error) {
       res.status((error as CustonError).status).json({ message: (error as Error).message });
+    }
+  }
+
+  validateLogin(req: Request, res: Response) {
+    try {
+      this._role = req.body;
+      return res.status(200).send(this._role);
+    } catch (error) {
+      console.log(error);
     }
   }
 }
