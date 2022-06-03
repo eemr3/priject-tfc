9a74dc20a284ac8abd673bc354b67b3755027bd3
eemr3 <eemr3@yahoo.com.br>
2022-06-03 18:59:45 -0300

    cria service para rota de teams

---
+++ app/backend/src/services/Team.service.ts
@@ -0,0 +1,22 @@
+import BaseError from '../utils/errorBase';
+import Team from '../database/models/team';
+import TTeams from './types/type.teams';
+
+class Teams {
+  private _users: Team[];
+  private _user: TTeams | null;
+
+  async getAll() {
+    this._users = await Team.findAll();
+    return this._users;
+  }
+
+  async getById(id: number) {
+    this._user = await Team.findOne({ where: { id } });
+    if (!this._user) throw new BaseError(400, 'Team not found');
+
+    return this._user;
+  }
+}
+
+export default Teams;
