f2c59d693c5200ba815cc328d421cf8c39ab0382
eemr3 <eemr3@yahoo.com.br>
2022-06-03 19:05:48 -0300

    feat: cria arquivo de mock para teams

---
+++ app/backend/src/tests/mocks/teamsMock.ts
@@ -0,0 +1,15 @@
+export const mockTeams = [
+  {
+    id: 1,
+    teamName: 'Avaí/Kindermann',
+  },
+  {
+    id: 2,
+    teamName: 'Bahia',
+  },
+];
+
+export const mockTeam = {
+  id: 1,
+  teamName: 'Avaí/Kindermann',
+};
