c5fa955edc51dd0ac12cd0fa8c6fd29b8ee18b55
eemr3 <eemr3@yahoo.com.br>
2022-06-03 19:05:15 -0300

    feat: adiciona rota teams

--- app/backend/src/routes/Routes.ts
+++ app/backend/src/routes/Routes.ts
@@ -3,9 +3,11 @@ import AuthMiddleware from '../middleware/authMiddleware';
 import ValidateJoi from '../middleware/validateJoiSchemas';
 import JoiSchemas from '../schemas/joiSchemas';
 import LoginController from '../controllers/Login.controller';
+import TeamController from '../controllers/Team.controller';
 
 class Routes {
-  private _controller = new LoginController();
+  private _controllerLogin = new LoginController();
+  private _controllerTeams = new TeamController();
   private _joiSchemas = new JoiSchemas();
   private _validate = new ValidateJoi();
   private _auth = new AuthMiddleware();
@@ -14,13 +16,19 @@ class Routes {
     app.post(
       '/login',
       (req, res, next) => this._validate.validate(req, res, next, this._joiSchemas.login),
-      (req, res) => this._controller.loginApp(req, res),
+      (req, res) => this._controllerLogin.loginApp(req, res),
     );
+
     app.get(
       '/login/validate',
       (req, res, next) => this._auth.validate(req, res, next),
-      (req, res) => this._controller.validateLogin(req, res),
+      (req, res) => this._controllerLogin.validateLogin(req, res),
     );
+  }
+
+  teams(app: express.Application) {
+    app.get('/teams/:id', (req, res) => this._controllerTeams.getById(req, res));
+    app.get('/teams', (req, res) => this._controllerTeams.getAll(req, res));
   }
 }
 
