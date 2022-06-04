f5b3d55d66a00cd812945713dccdb4452c8cb5b9
eemr3 <eemr3@yahoo.com.br>
2022-06-04 17:43:05 -0300

    feat: adiciona controller para a rota de post matches

--- app/backend/src/controllers/Matche.controller.ts
+++ app/backend/src/controllers/Matche.controller.ts
@@ -3,14 +3,30 @@ import MatcheService from '../services/Matche.service';
 
 class MatcheController {
   private _service = new MatcheService();
-  private _inprogress:boolean;
-  getAll = async (req: Request, res: Response) => {
+  private _inprogress: boolean;
+  async getAll(req: Request, res: Response) {
     const { inProgress } = req.query;
     this._inprogress = inProgress === 'true';
     const matches = await this._service.getAll(this._inprogress);
 
     return res.status(200).json(matches);
-  };
+  }
+
+  async createMatche(req: Request, res: Response) {
+    const {
+      homeTeam, awayTeam, homeTeamGoals, awayTeamGoals,
+      inProgress } = req.body;
+    console.log(req.body);
+    const matche = await this._service.createMatche({
+      homeTeam,
+      awayTeam,
+      homeTeamGoals,
+      awayTeamGoals,
+      inProgress,
+    });
+
+    return res.status(201).json(matche);
+  }
 }
 
 export default MatcheController;
