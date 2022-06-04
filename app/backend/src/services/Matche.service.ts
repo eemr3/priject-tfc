e1d030f6133b24084cab6771f23035a94c2d244a
eemr3 <eemr3@yahoo.com.br>
2022-06-04 17:41:58 -0300

    feat: adiciona metodo createMatche para criar novas partidas no BD

--- app/backend/src/services/Matche.service.ts
+++ app/backend/src/services/Matche.service.ts
@@ -1,9 +1,10 @@
 import Team from '../database/models/team';
 import Matche from '../database/models/matche';
+import TMatche from './types/type.matche';
 
 class MatcheService {
   private _matches: Matche[];
-
+  private _matche: Matche;
   async getAll(inProgress: boolean) {
     if (inProgress) {
       this._matches = await Matche.findAll({
@@ -22,6 +23,21 @@ class MatcheService {
       ],
     });
     return this._matches;
+  }
+
+  async createMatche(dataMatche: TMatche) {
+    const {
+      homeTeam,
+      awayTeam,
+      homeTeamGoals,
+      awayTeamGoals,
+      inProgress,
+    } = dataMatche;
+    this._matche = await Matche.create({
+      homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress,
+    });
+
+    return this._matche;
   }
 }
 
