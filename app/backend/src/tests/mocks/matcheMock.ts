2499c6236355b053b4ea39b3097d08c44e9c37c9
eemr3 <eemr3@yahoo.com.br>
2022-06-04 00:33:07 -0300

    feat: cria arquivo com mocks de retorno da rota matches

---
+++ app/backend/src/tests/mocks/matcheMock.ts
@@ -0,0 +1,106 @@
+export const matchesMock = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'São Paulo',
+    },
+    teamAway: {
+      teamName: 'Grêmio',
+    },
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'Internacional',
+    },
+    teamAway: {
+      teamName: 'Santos',
+    },
+  },
+  {
+    id: 3,
+    homeTeam: 4,
+    homeTeamGoals: 3,
+    awayTeam: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    teamHome: {
+      teamName: 'Corinthians',
+    },
+    teamAway: {
+      teamName: 'Napoli-SC',
+    },
+  },
+];
+
+export const matcheMockQueryTrue = [
+  {
+    id: 41,
+    homeTeam: 16,
+    homeTeamGoals: 2,
+    awayTeam: 9,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      teamName: 'São Paulo',
+    },
+    teamAway: {
+      teamName: 'Internacional',
+    },
+  },
+  {
+    id: 42,
+    homeTeam: 6,
+    homeTeamGoals: 1,
+    awayTeam: 1,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      teamName: 'Ferroviária',
+    },
+    teamAway: {
+      teamName: 'Avaí/Kindermann',
+    },
+  },
+];
+
+export const matcheMockQueryFalse = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'São Paulo',
+    },
+    teamAway: {
+      teamName: 'Grêmio',
+    },
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: 'Internacional',
+    },
+    teamAway: {
+      teamName: 'Santos',
+    },
+  },
+];
