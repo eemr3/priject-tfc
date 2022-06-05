be0de82138f953bb4504db50386c841469fdd9ee
eemr3 <eemr3@yahoo.com.br>
2022-06-05 17:03:56 -0300

    refactor: altera ferificação se existe no BD removendo do metodo

--- app/backend/src/services/Team.service.ts
+++ app/backend/src/services/Team.service.ts
@@ -1,4 +1,4 @@
-import BaseError from '../utils/errorBase';
+// import BaseError from '../utils/errorBase';
 import Team from '../database/models/team';
 import TTeams from './types/type.teams';
 
@@ -13,7 +13,7 @@ class Teams {
 
   async getById(id: number) {
     this._user = await Team.findOne({ where: { id } });
-    if (!this._user) throw new BaseError(400, 'Team not found');
+    // if (!this._user) throw new BaseError(404, 'There is no team with such id!');
 
     return this._user;
   }
