bf28feab1821f47557b334ab167a4974ae0c30b6
eemr3 <eemr3@yahoo.com.br>
2022-06-07 21:52:08 -0300

    refactor: remove linhas

--- app/backend/src/services/Team.service.ts
+++ app/backend/src/services/Team.service.ts
@@ -1,4 +1,3 @@
-// import BaseError from '../utils/errorBase';
 import Team from '../database/models/team';
 import TTeams from './types/type.teams';
 
@@ -13,7 +12,6 @@ class Teams {
 
   async getById(id: number) {
     this._user = await Team.findOne({ where: { id } });
-    // if (!this._user) throw new BaseError(404, 'There is no team with such id!');
 
     return this._user;
   }
